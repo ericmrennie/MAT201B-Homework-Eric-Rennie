4c41df2d9d5ecddeda96aa1d7c728ac6a3778c9b
ericmrennie <erennie97@gmail.com>
2026-04-16 13:33:17 -0700

    files deleted and gitignore fixed

--- build/p1/Release/CMakeFiles/p1_run_debug.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/p1_run_debug"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/p1_run_debug.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/p3/Release/CMakeFiles/p3_run.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/p3_run"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/p3_run.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
